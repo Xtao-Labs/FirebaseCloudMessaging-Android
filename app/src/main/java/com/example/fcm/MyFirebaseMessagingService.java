be07b084a9dc9a591ff852622428ffd379a3d3ff
xtaodada <xtao@xtaolink.cn>
2023-08-03 17:42:06 +0800

    xtao fcm

--- app/src/main/java/com/example/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/fcm/MyFirebaseMessagingService.java
@@ -3,56 +3,93 @@ package com.example.fcm;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.os.Build;
-import android.os.Bundle;
 import androidx.core.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.Map;
 
+/**
+ * NOTE: There can only be one service in each app that receives FCM messages. If multiple
+ * are declared in the Manifest then the first one will be chosen.
+ *
+ * In order to make this Java sample functional, you must remove the following from the Kotlin messaging
+ * service in the AndroidManifest.xml:
+ *
+ * <intent-filter>
+ *   <action android:name="com.google.firebase.MESSAGING_EVENT" />
+ * </intent-filter>
+ */
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 	public static final String FCM_PARAM = "picture";
 	private static final String CHANNEL_NAME = "FCM";
 	private static final String CHANNEL_DESC = "Firebase Cloud Messaging";
+	private static final String TAG = "MyFirebaseMsgService";
 	private int numMessages = 0;
 
 	@Override
 	public void onMessageReceived(RemoteMessage remoteMessage) {
-		super.onMessageReceived(remoteMessage);
-		RemoteMessage.Notification notification = remoteMessage.getNotification();
-		Map<String, String> data = remoteMessage.getData();
-		Log.d("FROM", remoteMessage.getFrom());
-		sendNotification(notification, data);
+		// [START_EXCLUDE]
+		// There are two types of messages data messages and notification messages. Data messages
+		// are handled
+		// here in onMessageReceived whether the app is in the foreground or background. Data
+		// messages are the type
+		// traditionally used with GCM. Notification messages are only received here in
+		// onMessageReceived when the app
+		// is in the foreground. When the app is in the background an automatically generated
+		// notification is displayed.
+		// When the user taps on the notification they are returned to the app. Messages
+		// containing both notification
+		// and data payloads are treated as notification messages. The Firebase console always
+		// sends notification
+		// messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+		// [END_EXCLUDE]
+
+		// TODO(developer): Handle FCM messages here.
+		// Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+		Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+		// Check if message contains a data payload.
+		if (remoteMessage.getData().size() > 0) {
+			Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+			handleNow();
+		}
+
+		// Check if message contains a notification payload.
+		if (remoteMessage.getNotification() != null) {
+			Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+			String notificationBody = remoteMessage.getNotification().getBody();
+			if (remoteMessage.getNotification().getBody() != null) {
+				sendNotification(remoteMessage.getNotification());
+			}
+		}
+
+		// Also if you intend on generating your own notifications as a result of a received FCM
+		// message, here is where that should be initiated. See sendNotification method below.
+	}
+	// [END receive_message]
+
+	@Override
+	public void onNewToken(String token) {
+		Log.d(TAG, "Refreshed token: " + token);
 	}
 
-	private void sendNotification(RemoteMessage.Notification notification, Map<String, String> data) {
-		Bundle bundle = new Bundle();
-		bundle.putString(FCM_PARAM, data.get(FCM_PARAM));
+	private void handleNow() {
+		Log.d(TAG, "Short lived task is done.");
+	}
 
-		Intent intent = new Intent(this, SecondActivity.class);
-		intent.putExtras(bundle);
-
-		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
+	private void sendNotification(RemoteMessage.Notification notification) {
 		NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, getString(R.string.notification_channel_id))
 				.setContentTitle(notification.getTitle())
-				.setContentText(notification.getBody())
+				.setStyle(new NotificationCompat.BigTextStyle().bigText(notification.getBody()))
 				.setAutoCancel(true)
 				.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-				//.setSound(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.win))
-				.setContentIntent(pendingIntent)
 				.setContentInfo("Hello")
 				.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
 				.setColor(getColor(R.color.colorAccent))
@@ -60,22 +97,9 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 				.setDefaults(Notification.DEFAULT_VIBRATE)
 				.setNumber(++numMessages)
 				.setSmallIcon(R.drawable.ic_notification);
-
-		try {
-			String picture = data.get(FCM_PARAM);
-			if (picture != null && !"".equals(picture)) {
-				URL url = new URL(picture);
-				Bitmap bigPicture = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-				notificationBuilder.setStyle(
-						new NotificationCompat.BigPictureStyle().bigPicture(bigPicture).setSummaryText(notification.getBody())
-				);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		final Notification app_notification = notificationBuilder.build();
 
 		NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 			NotificationChannel channel = new NotificationChannel(
 					getString(R.string.notification_channel_id), CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT
@@ -91,8 +115,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 			assert notificationManager != null;
 			notificationManager.createNotificationChannel(channel);
 		}
-
 		assert notificationManager != null;
-		notificationManager.notify(0, notificationBuilder.build());
+		notificationManager.notify(0, app_notification);
 	}
 }
